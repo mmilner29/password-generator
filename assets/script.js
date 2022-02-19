582097d54a5f55327ad099e299be46393d31e108
mmilner29 <madisonm4514@gmail.com>
2022-02-18 20:37:46 -0700

    add if statements for character combinations

--- assets/script.js
+++ assets/script.js
@@ -5,23 +5,67 @@ var confirmNumbers;
 var characters;
 
 //Lowercase letters
-letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //Lowercase & Uppercase letters
 
-lettersCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lettersCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 //Numbers
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 //Special Characters
-specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+const specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+//Combination of characters
+var combinedCharacters;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 //Write Generate Password Function
+function generatePassword() {
+  length = parseInt(window.prompt("How many characters would you like your password to be?"));
+  console.log(length);
 
+  if (length === "" || null) {
+    window.alert("You need to provide a valid answer! Please try again");
+    return generatePassword();
+  } else if (length > 0) {
+    //desired input
+    capital = confirm ("Would you like capital letters included in your password?");
+    confirmNumbers = confirm ("Would you like numbers included in your password?");
+    characters = confirm ("Would you like special characters included in your password?");
+  } else {
+    window.alert("You need to provide a valid answer! Please try again");
+    return generatePassword();
+  }
+
+  //Combinations of situations
+  if (!capital && !confirmNumbers && !characters) {
+    combinedCharacters = letters;
+  } else if (capital && confirmNumbers && characters) {
+    combinedCharacters = lettersCases.concat(numbers, specialCharacters);
+  } 
+  
+    else if (!capital && !confirmNumbers && characters) {
+    combinedCharacters = letters.concat(specialCharacters);
+  } else if (!capital && confirmNumbers && !characters) {
+    combinedCharacters = letters.concat(numbers);
+  } else if (capital && !confirmNumbers && !characters) {
+    combinedCharacters = lettersCases;
+  } 
+  
+  else if (!capital && confirmNumbers && characters) {
+    combinedCharacters = letters.concat(numbers, specialCharacters);
+  } else if (capital && confirmNumbers && !characters) {
+    combinedCharacters = lettersCases.concat(numbers);
+  } else if (capital && !confirmNumbers && characters) {
+    combinedCharacters = specialCharacters.concat(lettersCases);
+  };
+
+return combinedCharacters;
+};
 
 // Write password to the #password input
 function writePassword() {
