53272950e57c5a28d249ff1b00a2e647d57110c5
mmilner29 <madisonm4514@gmail.com>
2022-02-18 19:24:56 -0700

    organize js

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Password Generator Starter Code
+# Password Generator 
+
+## Purpose
+A website that generates a random password based on user input
+
+## Built With
+* HTML
+* CSS
+* Javascript
+
+## Website
+
+
+
+## Preview
+
+![Website Preview](/assets/images/portfolio-preview.png)
+
+## Contribution
+Made with ❤️ by Madi Milner
--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,27 @@
-// Assignment code here
+//Define user input variables
+var length;
+var capital;
+var confirmNumbers;
+var characters;
 
+//Lowercase letters
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Lowercase & Uppercase letters
+
+lettersCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//Numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//Special Characters
+specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+//Write Generate Password Function
+
 
 // Write password to the #password input
 function writePassword() {
